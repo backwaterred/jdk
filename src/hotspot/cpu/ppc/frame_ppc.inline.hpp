6d52cfc12f46ca40b107e035c998ef65c0240cfa
Tyler Steele <tyler.steele@ibm.com>
2023-02-14 15:20:36 -0700

    AIX min stack args for compiled frame is 8 PWs

--- src/hotspot/cpu/ppc/frame_ppc.inline.hpp
+++ src/hotspot/cpu/ppc/frame_ppc.inline.hpp
@@ -364,7 +364,13 @@ inline const ImmutableOopMap* frame::get_oop_map() const {
 
 inline int frame::compiled_frame_stack_argsize() const {
   assert(cb()->is_compiled(), "");
-  return (cb()->as_compiled_method()->method()->num_stack_arg_slots() * VMRegImpl::stack_slot_size) >> LogBytesPerWord;
+  #if !defined(ELF_ABIv2)
+    return (cb()->as_compiled_method()->method()->num_stack_arg_slots() * VMRegImpl::stack_slot_size) >> LogBytesPerWord;
+  #else
+    // On AIX, space for 8 PWs (stack args) is allocated regardless of the actual number of arguments.
+    // TODO: Magic number are bad!
+    return (MAX2(cb()->as_compiled_method()->method()->num_stack_arg_slots(),8) * VMRegImpl::stack_slot_size) >> LogBytesPerWord;
+  #endif
 }
 
 inline void frame::interpreted_frame_oop_map(InterpreterOopMap* mask) const {
