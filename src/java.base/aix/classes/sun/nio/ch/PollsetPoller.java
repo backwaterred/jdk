d3d4817017cce8690456ea3c5531b7925ff5d4c8
Tyler Steele <tyler.steele@ibm.com>
2023-03-30 15:51:32 -0600

    VThread AIX Baseline
    - implement missing PollsetPoller
    - change globals_ppc: VMCont = true
    - tweak src/hotspot/share/adlc/main.cpp

--- src/java.base/aix/classes/sun/nio/ch/PollsetPoller.java
+++ src/java.base/aix/classes/sun/nio/ch/PollsetPoller.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,39 +25,62 @@
 package sun.nio.ch;
 
 import java.io.IOException;
+import sun.nio.ch.AixPollPort;
 
 /**
  * Poller implementation based on the AIX Pollset library.
  */
 
 class PollsetPoller extends Poller {
+    private int setid;
+    private int setsize;
 
     PollsetPoller(boolean read) throws IOException {
         super(read);
+        this.setsize = 0;
+        this.setid = AixPollPort.pollsetCreate();
     }
 
     @Override
     int fdVal() {
-        // Stub
-        throw new UnsupportedOperationException("Unimplemented on AIX");
+        return setid;
     }
 
     @Override
-    void implRegister(int fdVal) throws IOException {
-        // Stub
-        throw new UnsupportedOperationException("Unimplemented on AIX");
+    void implRegister(int fd) throws IOException {
+        setsize++;
+
+        int ret = AixPollPort.pollsetCtl(setid, AixPollPort.PS_MOD, fd,
+                                         this.reading() ? Net.POLLIN : Net.POLLOUT);
+        if (ret != 0) {
+            throw new IOException("Unable to register fd " + fd +
+                                    ". Command failed with ERRNO " + ret);
+        }
     }
 
     @Override
-    void implDeregister(int fdVal) {
-        // Stub
-        throw new UnsupportedOperationException("Unimplemented on AIX");
+    void implDeregister(int fd) {
+        setsize--;
+
+        int ret = AixPollPort.pollsetCtl(setid, AixPollPort.PS_DELETE, fd, 0);
+
+        assert ret == 0;
     }
 
     @Override
-    int poll(int timeout) throws IOException {
-        // Stub
-        throw new UnsupportedOperationException("Unimplemented on AIX");
+    int poll(int _timeout) throws IOException {
+        // TODO: use timeout (pollset_poll_ext call takes timeout value)
+        long buffer = AixPollPort.allocatePollArray(setsize);
+
+        int n = AixPollPort.pollsetPoll(setid, buffer, setsize);
+        for(int i=0; i<n; i++) {
+            long eventAddress = AixPollPort.getEvent(buffer, i);
+            int fd = AixPollPort.getDescriptor(eventAddress);
+            polled(fd);
+        }
+
+        AixPollPort.freePollArray(buffer);
+        return n;
     }
 }
 
